9e17b33805ce2c230538c52a13bda9dba8db1210
South Matic <Southmatic729@gmail.com>
2019-10-06 20:55:39 -0400

    eslint formatted

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,158 +1,176 @@
-/*eslint-env jquery*/
-$(document).ready(function() {
-  var wins = 0;
-  var losses = 0;
-  var player = "";
-  var player1present = false;
-  var player2present = false;
+/* eslint-env jquery */
+$(document).ready(function () {
+  var wins = 0
+  var losses = 0
+  var player = ''
+  var player1present = false
+  var player2present = false
 
   var firebaseConfig = {
-    apiKey: "AIzaSyAa7lMljbrokwSwsctL_l9FcdyIvS6KuQ8",
-    authDomain: "armon-rps.firebaseapp.com",
-    databaseURL: "https://armon-rps.firebaseio.com",
-    projectId: "armon-rps",
-    storageBucket: "",
-    messagingSenderId: "516504842409",
-    appId: "1:516504842409:web:b4290bdceddf00b53b79b8"
-  };
+    apiKey: 'AIzaSyAa7lMljbrokwSwsctL_l9FcdyIvS6KuQ8',
+    authDomain: 'armon-rps.firebaseapp.com',
+    databaseURL: 'https://armon-rps.firebaseio.com',
+    projectId: 'armon-rps',
+    storageBucket: '',
+    messagingSenderId: '516504842409',
+    appId: '1:516504842409:web:b4290bdceddf00b53b79b8'
+  }
 
-  firebase.initializeApp(firebaseConfig);
-  var database = firebase.database();
-  console.log(database);
+  firebase.initializeApp(firebaseConfig)
+  var database = firebase.database()
+  console.log(database)
   var gameDatabase = firebase
     .database()
     .ref()
-    .child("gameObject");
+    .child('gameObject')
   var trashDatabase = firebase
     .database()
     .ref()
-    .child("trashtalk");
+    .child('trashtalk')
   var onlineDatabase = firebase
     .database()
     .ref()
-    .child("presence");
-  var firstcomment = 0;
-  trashDatabase.on("child_added", function(snap) {
-    console.log("snapshot", snap.val());
-    console.log("snapshot of date", snap.Date);
-    console.log("snapshot", snap);
-    console.log("snapshot", snap);
-    console.log("snapshot", snap);
+    .child('presence')
+  var firstcomment = 0
+  trashDatabase.on('child_added', function (snap) {
+    var newEntry = $('<tr>')
+    var newTrash = $('<th>')
+    var newDate = $('<th>')
+    var newName = $('<td>')
+    newDate.attr('scope', 'row')
+    newDate.text(snap.val().Date)
+    newEntry.append(newDate)
+    newName.text(snap.val().Name)
+    newEntry.append(newName)
+    newTrash.text(snap.val().Trash)
+    newEntry.append(newTrash)
+    $('tbody').prepend(newEntry)
+    firstcomment++
+    if (firstcomment > 3) {
+      $('tr:last').remove()
+    }
+  },
+  function (errorObject) {
+    console.log('Errors handled: ' + errorObject.code)
+  })
 
-    var newEntry = $("<tr>");
-    var newTrash = $("<th>");
-    var newDate = $("<th>");
-    var newName = $("<td>");
-    newDate.attr("scope", "row");
-    newDate.text(snap.val().Date);
-    newEntry.append(newDate);
-    newName.text(snap.val().Name);
-    newEntry.append(newName);
-    newTrash.text(snap.val().Trash);
-    newEntry.append(newTrash);
-    // newEntry.append(newDat
-    $("tbody").prepend(newEntry);
-    firstcomment++;
-    console.log();
-    if (firstcomment > 4) {
-      $("tr:last").remove();
+  gameDatabase.on('value', function (snap) {
+    console.log('game', snap.val())
+  },
+  function (errorObject) {
+    console.log('Errors handled: ' + errorObject.code)
+  })
+
+  onlineDatabase.on('value', function (snap) {
+    console.log('presence', snap.val())
+    player1present = snap.val().player1
+    player2present = snap.val().player2
+    console.log('player 1 is ', player1present)
+    console.log('player 2 is ', player2present)
+    firstpick()
+  },
+  function (errorObject) {
+    console.log('Errors handled: ' + errorObject.code)
+  })
+
+  function animateCSS (element, animationName, callback) {
+    const node = document.querySelector(element)
+    node.classList.add('animated', animationName)
+
+    function handleAnimationEnd () {
+      node.classList.remove('animated', animationName)
+      node.removeEventListener('animationend', handleAnimationEnd)
+
+      if (typeof callback === 'function') callback()
     }
 
-    console.log("trash talk", snap.val());
-  });
-  gameDatabase.on("value", function(snap) {
-    console.log("game", snap.val());
-  });
-  onlineDatabase.on("value", function(snap) {
-    console.log("presence", snap.val());
-    player1present = snap.val().player1;
-    player2present = snap.val().player2;
-    console.log("player 1 is ", player1present);
-    console.log("player 2 is ", player2present);
-  });
-
-  function animateCSS(element, animationName, callback) {
-    const node = document.querySelector(element);
-    node.classList.add("animated", animationName);
-
-    function handleAnimationEnd() {
-      node.classList.remove("animated", animationName);
-      node.removeEventListener("animationend", handleAnimationEnd);
-
-      if (typeof callback === "function") callback();
-    }
-
-    node.addEventListener("animationend", handleAnimationEnd);
+    node.addEventListener('animationend', handleAnimationEnd)
   }
 
-  var connectionsRef = database.ref("/connections");
+  var connectionsRef = database.ref('/connections')
 
   // '.info/connected' is a special location provided by Firebase that is updated
   // every time the client's connection state changes.
   // '.info/connected' is a boolean value, true if the client is connected and false if they are not.
-  var connectedRef = database.ref(".info/connected");
+  var connectedRef = database.ref('.info/connected')
 
   // When the client's connection state changes...
-  connectedRef.on("value", function(snap) {
-    // If they are connected..
-    if (snap.val()) {
-      // Add user to the connections list.
-      var con = connectionsRef.push(true);
-      // Remove user from the connection list when they disconnect.
-      con.onDisconnect().remove();
+  connectedRef.on(
+    'value',
+    function (snap) {
+      // If they are connected..
+      if (snap.val()) {
+        // Add user to the connections list.
+        var con = connectionsRef.push(true)
+        // Remove user from the connection list when they disconnect.
+        con.onDisconnect().remove()
+      }
+    },
+    function (errorObject) {
+      console.log('Errors handled: ' + errorObject.code)
     }
-  });
+  )
 
   // When first loaded or when the connections list changes...
-  connectionsRef.on("value", function(snap) {
-    var present = snap.numChildren();
-    console.log(present);
-    console.log(typeof present);
-    // Display the viewer count in the html.
-    // The number of online users is the number of children in the connections list.
-    $("#connected-viewers").text(present);
+  connectionsRef.on('value', function (snap) {
+    var present = snap.numChildren()
+
+    $('#connected-viewers').text(present)
     if (present === 1) {
-      animateCSS("#player1", "bounce");
-      player = "player1";
-      player1present = true;
-      player2present = false;
-      console.log("I am ", player);
+      animateCSS('#player1', 'bounce')
+      player = 'player1'
+      onlineDatabase.set({
+        player1: true,
+        player2: false
+      })
+      console.log('I am ', player)
     } else if (present === 2) {
-      animateCSS("#player1", "bounce");
-      animateCSS("#player2", "bounce");
-      player = "player2";
-      player2present = true;
-      console.log("i am ", player);
-      resetTrashTalk();
-      letsstartTrashing();
+      // animateCSS('#player1', 'bounce')
+      animateCSS('#player2', 'bounce')
+      player = 'player2'
+      onlineDatabase.set({
+        player1: true,
+        player2: true
+      })
+      console.log('i am ', player)
+      resetTrashTalk()
+      letsstartTrashing()
+      firstpick()
     }
-  });
-  function resetTrashTalk() {
-    trashDatabase.set({});
+  }, function (errorObject) {
+    console.log('Errors handled: ' + errorObject.code)
+  })
+
+  function resetTrashTalk () {
+    trashDatabase.set({})
   }
 
-  function firstpick() {
-    $(".deck").on("click", function() {
-      var value = this.id;
-      console.log(value);
-    });
+  function firstpick () {
+    if (player1present && player2present) {
+      $('.weapons').on('click', function () {
+        var value = this.id
+        console.log(value)
+      })
+    }
   }
 
-  function letsstartTrashing() {
-    $("button").on("click", function(event) {
-      event.preventDefault();
-      var comment = $("#snap").val();
-      var whoSaidit = $("#playerName").val();
-      var time = new Date();
-      var now = time.toLocaleTimeString();
-      console.log("time", now);
-      console.log("name", whoSaidit);
-      console.log("comment", comment);
+  function letsstartTrashing () {
+    $('button').on('click', function (event) {
+      event.preventDefault()
+      var comment = $('#snap').val()
+      var whoSaidit = $('#playerName').val()
+      var time = new Date()
+      var now = time.toLocaleTimeString()
+      console.log('time', now)
+      console.log('name', whoSaidit)
+      console.log('comment', comment)
       trashDatabase.push({
         Date: now,
         Name: whoSaidit,
         Trash: comment
-      });
-    });
+      })
+    }, function (errorObject) {
+      console.log('Errors handled: ' + errorObject.code)
+    })
   }
-});
+})
